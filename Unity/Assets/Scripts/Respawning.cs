41e6910831247390e039383af7c968bf03d8e79b
bmthtdwp <daxterr1@hotmail.com>
2018-03-27 12:50:18 +0200

    Added waypoint system
    Check for one thats closest by and respwan there. ( still needs some fixing )

---
+++ Unity/Assets/Scripts/Respawning.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class Respawning : MonoBehaviour
+{
+    [SerializeField] private Waypoints[] waypoints;
+    [SerializeField] private Transform tMin;
+    private Collision _col;
+
+    void Start()
+    {
+        waypoints = FindObjectsOfType<Waypoints>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        _col = other;
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            Transform[] tempTranform = waypoints[i].GetComponents<Transform>();
+            GetClosestWaypoint(tempTranform, other.transform.position);
+        }      
+    }
+
+    void Respawn(Transform respawnPos)
+    {
+        if(_col!= null)
+        _col.transform.position = respawnPos.position;
+    }
+
+    Transform GetClosestWaypoint(Transform[] waypointPos, Vector3 player)
+    {
+        tMin = null;
+        float minDist = Mathf.Infinity;
+        Vector3 playerPos = player;
+        foreach (Transform t in waypointPos)
+        {
+            float dist = Vector3.Distance(playerPos, t.position);
+            if (dist < minDist)
+            {
+                tMin = t;
+                minDist = dist;
+            }
+        }
+        Respawn(tMin);
+        _col = null;
+        return tMin;     
+    }
+}
