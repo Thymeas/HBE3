6fc887945c25779a443e426a4e39fb106fc25484
bmthtdwp <daxterr1@hotmail.com>
2018-03-28 09:53:40 +0200

    Added positionTracker
    Tracks distance between waypoints and players

--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -2,21 +2,40 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float _startSpeed = 20.0f, _maxSpeed = 75.0f;
     [SerializeField] private float _moveSpeed;
+    private float _startSpeed = 20.0f, _maxSpeed = 75.0f;
     public PlayerMovement[] players;
+
+    public Waypoints[] _waypoint;
+    public int counter;
+    [SerializeField] private float distance = 2.0f;
 
     private void Start()
     {
         _moveSpeed = _startSpeed;
         players = FindObjectsOfType<PlayerMovement>();
+        _waypoint = FindObjectsOfType<Waypoints>();
     }
 
     void Update()
     {
+        CalculateNextWaypoint();
         MoveForward(Input.GetAxis("Vertical"));
         Rotate(Input.GetAxis("Horizontal"));
-        Jump(Input.GetKeyDown(KeyCode.Space));
+        Jump(Input.GetKeyDown(KeyCode.Space));     
+    }
+
+    private void CalculateNextWaypoint()
+    {
+        var direction = Vector3.zero;
+        direction = _waypoint[counter].transform.position - transform.position;
+        if (direction.magnitude < distance)
+        {
+            if (counter < _waypoint.Length - 1)
+                counter++;
+            else
+                counter = 0;
+        }
     }
 
     void MoveForward(float input)
@@ -44,4 +63,6 @@ public class PlayerMovement : MonoBehaviour
         if (isPressingJump && Physics.Raycast(transform.position, Vector3.down, 0.5f))
                 transform.GetComponent<Rigidbody>().AddForce(0, 200, 0);
     }
+
+    
 }
---
+++ Unity/Assets/Scripts/PositionTracker.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PositionTracker : MonoBehaviour
+{
+    [SerializeField] private Waypoints[] _waypoints;
+    [SerializeField] private PlayerMovement[] _players;
+    [SerializeField] private int _counter;
+    private float _fraction;
+
+    void Start()
+    {
+        _players = FindObjectsOfType<PlayerMovement>();
+        _waypoints = FindObjectsOfType<Waypoints>();
+    }
+
+    void Update()
+    {
+        UpdateCounter();
+        CalculatePlayerFraction();
+    }
+
+    private void UpdateCounter()
+    {
+        _counter = FindObjectOfType<PlayerMovement>().counter;
+    }
+
+    private void CalculatePlayerFraction()
+    {
+        foreach (PlayerMovement t in _players)
+        {
+            GetFractionOfPathCovered(t.transform.position, _waypoints[_counter].transform.position,
+                _waypoints[_counter + 1].transform.position);
+        }
+    }
+
+    private float GetFractionOfPathCovered(Vector3 playerPos, Vector3 lastWaypointReached, Vector3 nextWaypoint)
+    {
+        Vector3 displacementFromCurrentWaypoint = playerPos - lastWaypointReached;
+        Vector3 currentSegmentVector = nextWaypoint - lastWaypointReached;
+        _fraction = Vector3.Dot(displacementFromCurrentWaypoint, currentSegmentVector) /
+                         currentSegmentVector.sqrMagnitude;
+        return _fraction;
+    }
+}
