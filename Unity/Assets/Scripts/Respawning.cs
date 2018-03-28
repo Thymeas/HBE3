4830dcdef3ab63781927cdb746317b5cb0c5d82e
bmthtdwp <daxterr1@hotmail.com>
2018-03-28 14:48:18 +0200

    -

--- Unity/Assets/Scripts/Respawning.cs
+++ Unity/Assets/Scripts/Respawning.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using UnityEngine;
-using UnityEngine.Experimental.UIElements;
 
 public class Respawning : MonoBehaviour
 {
